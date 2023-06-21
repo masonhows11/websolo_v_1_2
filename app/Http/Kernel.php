0324d7c2ea2ff0b90248bfbfb966235abc34d41a
mason.hows11 <mason.hows11@gmail.com>
2023-06-21 17:51:27 +0330

    add admin auth views

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -54,8 +54,8 @@ class Kernel extends HttpKernel
      * @var array<string, class-string|string>
      */
     protected $routeMiddleware = [
-        'auth' => \App\Http\Middleware\Authenticate::class,
-        'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+        'auth_front' => \App\Http\Middleware\Authenticate::class,
+        'auth_front.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
