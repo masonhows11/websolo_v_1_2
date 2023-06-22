2399b973c5a85bac2badcc34698ed8c73dfec0cd
mason.hows11 <mason.hows11@gmail.com>
2023-06-22 21:30:16 +0330

    add auth view & routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\Auth\EmailVerifyPromptController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\VerifyEmailController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,3 +19,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [HomeController::class, 'home'])->name('home');
+
+// authentication & authorize
+Route::get('/login/form', [LoginController::class, 'loginForm'])->name('login.form');
+Route::post('/login', [LoginController::class, 'login'])->name('login');//->middleware(['throttle:3,1']);
+Route::get('/register/form', [RegisterController::class, 'registerForm'])->name('register.form');
+Route::post('/register', [RegisterController::class, 'register'])->name('register');
+Route::get('/emailVerifyPrompt', [EmailVerifyPromptController::class, 'verifyEmailPrompt'])->name('email.verify.prompt');
+Route::post('/resendVerifyEmail', [VerifyEmailController::class, 'resendEmailVerify'])->name('resend.verify.email');
+Route::get('/emailVerify/{id}/{code}', [VerifyEmailController::class, 'verifyEmail'])->name('email.verify');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
+});
