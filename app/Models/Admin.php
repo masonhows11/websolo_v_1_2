dbf9af1d74d67ae929feb09fa480a1ef4f4a0d5e
mason.hows11 <mason.hows11@gmail.com>
2023-06-22 22:38:58 +0330

    add admin req

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -2,10 +2,6 @@
 
 namespace App\Models;
 
-<?php
-
-namespace App\Models;
-
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
