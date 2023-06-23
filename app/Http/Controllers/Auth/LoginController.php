1f2eb07cbc288b64514e31218fe0ace7d7094692
mason.hows11 <mason.hows11@gmail.com>
2023-06-23 23:23:03 +0330

    logout has problem

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -7,7 +7,6 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Crypt;
 use Illuminate\Support\Str;
 
 class LoginController extends Controller
--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -15,7 +15,12 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
-            return route('login');
+
+            if ($request->routeIs('admin.*')){
+                return route('login_admin_form');
+
+            }
+            return route('login.form');
         }
     }
 }
