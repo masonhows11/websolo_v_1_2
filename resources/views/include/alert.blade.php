04bc7dcff09d3065fd27af9e2ec833cf2becc1a3
mason.hows11 <mason.hows11@gmail.com>
2023-06-23 18:51:06 +0330

    add alert fade script

--- resources/views/include/alert.blade.php
+++ resources/views/include/alert.blade.php
@@ -1,13 +1,13 @@
 @if(session('success'))
     <div class="alert alert-success alert-dismissible alert-component">
         <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
-        <strong>{{ session('success') }}</strong>
+        <strong class="text-center">{{ session('success') }}</strong>
     </div>
 @endif
 @if(session('error'))
     <div class="alert alert-danger alert-dismissible alert-component">
         <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
-        <strong>{{ session('error') }}</strong>
+        <strong class="text-center">{{ session('error') }}</strong>
     </div>
 @endif
 
--- resources/views/include/scripts.blade.php
+++ resources/views/include/scripts.blade.php
@@ -1,1 +1,6 @@
 <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+<script>
+    $(document).ready(function() {
+        $('.alert-component').delay('3000').fadeOut();
+    })
+</script>
