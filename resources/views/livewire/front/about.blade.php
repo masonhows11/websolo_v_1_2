28a68f68d2ebc2ed6a9d327b8b5e0d5fe5e3b02d
mason.hows11 <mason.hows11@gmail.com>
2023-06-24 19:30:40 +0330

    about-single need style

--- resources/views/livewire/front/about.blade.php
+++ resources/views/livewire/front/about.blade.php
@@ -2,88 +2,98 @@
     @section('page_title')
         درباره ما
     @endsection
-        <div class="container">
+    <div class="container">
 
-            <div class="about row mt-5">
+        <div class="about row mt-5">
 
-
-                <div class="about-img col-md-3">
-                    <img src="{{'images/avatar/avatar.jpg'}}" alt="" class="img-thumbnail mb-4"/>
-                </div>
-
-                <div class="about-introduce col-md-9">
-                    <h2 class="title h2 p-1 my-2">نعیم سلطانی</h2>
-                    <p class="subtitle p-1 my-2">برنامه نویس و توسعه دهنده سمت سرور و کاربر
-                        <span>(Programmer and full-stack develper)</span>
-                    </p>
-                    <p class="introduce p-1">
-                        کارشناس کامپیوتر(نرم افزار) از سال 1396 برنامه نویسی رو شروع کردم تا
-                        به امروز، علاقه من به تکنولوژی در هر زمینه ای که دوست داشته باشم
-                        مخصوصا کامپیوتر...
-                    </p>
-                    <button class="btn btn-resume rounded mt-3">دریافت رزومه</button>
-                </div>
-
+            <div class="about-img col-md-3">
+                <img src="{{'images/avatar/avatar.jpg'}}" alt="" class="img-thumbnail mb-4"/>
             </div>
 
-            <div class="row d-flex flex-column my-5  about-us-wrapper border border-1 border-secondary w3-flat-midnight-blue rounded-3">
+            <div class="about-introduce col-md-9">
+                <h2 class="title h2 p-1 my-2">نعیم سلطانی</h2>
+                <p class="subtitle p-1 my-2">برنامه نویس و توسعه دهنده سمت سرور و کاربر
+                    <span>(Programmer and full-stack develper)</span>
+                </p>
+                <p class="introduce p-1">
+                    کارشناس کامپیوتر(نرم افزار) از سال 1396 برنامه نویسی رو شروع کردم تا
+                    به امروز، علاقه من به تکنولوژی در هر زمینه ای که دوست داشته باشم
+                    مخصوصا کامپیوتر...
+                </p>
+                <button class="btn btn-resume rounded mt-3">دریافت رزومه</button>
+            </div>
 
-                <div class="col py-5 about-sec-one">
-                    <div class="row  intro-webkade row-cols-xxl-2 row-cols-xl-2 row-cols-lg-2 row-cols-md-1 row-cols-1">
-                        <div class="col intro-webkade-title  d-flex justify-content-start">
-                            <h1 class="we-title">وب سولو</h1>
-                        </div>
-                        <div class="col">
-                            <div class="we-description">
-                                <h3 class="text-center">درباره :</h3>
-                                <p>وبکده یک سایت یا یک جامعه کوچک از دنیای بزرگ برنامه نویسی شاخه <span
-                                        class="inline-text-intro">توسعه و طراحی وب سایت یا وب اپلیکیشن</span>
-                                    و سایر تکنولوژی های وابسته به این شاخه است.</p>
-                            </div>
-                        </div>
-                    </div>
+        </div>
+
+        <div class="row d-flex  my-5  border border-1   rounded-3">
+
+            <div class="col about-sec-one">
+
+                <div class="">
+                    <h1 class="we-title">وب سولو</h1>
                 </div>
 
-                <div class="col d-flex justify-content-center my-5 about-us-goal">
-                    <div class="d-flex flex-row">
-                        <i class="fas fa-campground fa-2x me-4 d-flex align-self-center"></i>
-                        <p class="d-flex align-self-center">اهداف وب سولو</p>
-                        <i class="fas fa-campground fa-2x ms-4 d-flex align-self-center"></i>
-                    </div>
-                </div>
-
-                <div class="col py-5 about-sec-two">
-                    <div class="row row-cols-xxl-2 row-cols-xl-2 row-cols-lg-2 row-cols-md-1 row-cols-1">
-                        <div class="col d-flex flex-row">
-                            <i class="fas fa-fire-alt ms-2"></i><h5 class="ms-2">ایجاد بسترهای مناسب جهت کسب و کارهای انلاین :</h5>
-                        </div>
-                        <div class="col">
-                            <p>
-                                تیم وبکده با صرف کمترین هزینه به جهت هر چه بهتر دیده شدن محصولات فعالان اقتصادی و خدمات شرکت های مشتری محور خدمات ارائه می نماید. تیم وبکده آماده ارائه جدیدترین خدمات در حوزه طراحی و توسعه سایت و اپلیکیشین های موبایل می باشد.
-                            </p>
-                        </div>
-                    </div>
-
-                </div>
-
-                <div class="col py-5 about-sec-three">
-                    <div class="row row-cols-xxl-2 row-cols-xl-2 row-cols-lg-2 row-cols-md-1 row-cols-1">
-                        <div class="col d-flex flex-row">
-                            <i class="fas fa-fire-alt ms-2"></i><h5 class="ms-2">ارائه آموزش های لازم و نکات کاربردی در زمینه توسعه وب سایت :</h5>
-                        </div>
-                        <div class="col">
-                            <p>
-                                بخش دیگری از سایت به آموزش اختصاص داده شده است.
-                            </p>
-                            <p>
-                                در این بخش تلاش می کنیم آموزش لازم و  کاربردی در زمینه های برنامه نویسی و توصعه وب سایت (web development & programming) و موبایل (mobile development)
-                                و طراحی کاربری (ui.ux)  را به صورت مفهومی و به زبانی ساده برای علاقه مندان ارائه نماییم.
-                                ما سعی میکنیم از طریق گسترش علم برنامه نویسی ولو از شاخه توسعه وب  دریچه های جدیدی ازاین علم را به روی همگان باز کنیم.
-                            </p>
-                        </div>
+                <div class="">
+                    <div class="we-description">
+                        <h3 class="text-center">درباره :</h3>
+                        <p>وبکده یک سایت یا یک جامعه کوچک از دنیای بزرگ برنامه نویسی شاخه توسعه و طراحی وب سایت یا وب
+                            اپلیکیشن و سایر تکنولوژی های وابسته به این شاخه است.</p>
                     </div>
                 </div>
 
             </div>
         </div>
+
+        <div class="row d-flex justify-content-center my-5 about-sec-two ">
+            <div class="d-flex flex-row">
+                <p class="d-flex align-self-center">اهداف وب سولو</p>
+            </div>
+        </div>
+
+        <div class="row py-5 about-sec-three">
+            <div class="row row-cols-xxl-2 row-cols-xl-2 row-cols-lg-2 row-cols-md-1 row-cols-1">
+                <div class="col d-flex flex-row">
+                    <i class="fas fa-fire-alt ms-2"></i><h5 class="ms-2">ایجاد بسترهای مناسب جهت کسب و کارهای انلاین
+                        :</h5>
+                </div>
+                <div class="col">
+                    <p>
+                        تیم وبکده با صرف کمترین هزینه به جهت هر چه بهتر دیده شدن محصولات فعالان اقتصادی و خدمات شرکت های
+                        مشتری محور خدمات ارائه می نماید. تیم وبکده آماده ارائه جدیدترین خدمات در حوزه طراحی و توسعه سایت
+                        و
+                        اپلیکیشین های موبایل می باشد.
+                    </p>
+                </div>
+            </div>
+
+        </div>
+
+
+        <div class="row py-5 about-sec-four">
+            <div class="row row-cols-xxl-2 row-cols-xl-2 row-cols-lg-2 row-cols-md-1 row-cols-1">
+                <div class="col d-flex flex-row">
+                    <i class="fas fa-fire-alt ms-2"></i><h5 class="ms-2">ارائه آموزش های لازم و نکات کاربردی در زمینه
+                        توسعه
+                        وب سایت :</h5>
+                </div>
+                <div class="col">
+                    <p>
+                        بخش دیگری از سایت به آموزش اختصاص داده شده است.
+                    </p>
+                    <p>
+                        در این بخش تلاش می کنیم آموزش لازم و کاربردی در زمینه های برنامه نویسی و توصعه وب سایت (web
+                        development & programming) و موبایل (mobile development)
+                        و طراحی کاربری (ui.ux) را به صورت مفهومی و به زبانی ساده برای علاقه مندان ارائه نماییم.
+                        ما سعی میکنیم از طریق گسترش علم برنامه نویسی ولو از شاخه توسعه وب دریچه های جدیدی ازاین علم را
+                        به
+                        روی همگان باز کنیم.
+                    </p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+
+</div>
 </div>
