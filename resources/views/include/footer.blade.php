0b2f12a538b8536b104d4a7a169053ecd4f9e84c
mason.hows11 <mason.hows11@gmail.com>
2023-06-24 21:02:42 +0330

    footer responsive updatewd

--- resources/views/include/footer.blade.php
+++ resources/views/include/footer.blade.php
@@ -1,33 +1,31 @@
 <section class="container footer-wrapper">
     <div class="page-footer">
         <div class="row">
+
             <div class="col-sm-6 d-flex justify-content-center align-items-center">
-                <p class="p-5">
-                    Copyright
-                    <script>
-                        document.write(new Date().getFullYear());
-                    </script>
-                    ©
-                    <a class="footer-copyright" href="#" target="_blank"
-                    >Mason_Hows11</a
-                    >
-                </p>
+                    <p class="py-3 text-center">Copyright
+                        <script>
+                            document.write(new Date().getFullYear());
+                        </script>
+                        ©
+                        <a class="footer-copyright" href="#" target="_blank">Mason_Hows11</a>
+                    </p>
             </div>
 
             <div class="col-sm-6 d-flex justify-content-center align-items-center">
                 <div class="socials d-flex flex-row p-5">
-                    <a class="social-item" href="javascript:void(0)"
-                    ><i class="fa-brands fa-facebook-f"></i
-                        ></a>
-                    <a class="social-item" href="javascript:void(0)"
-                    ><i class="fa-brands fa-google-plus-g"></i
-                        ></a>
-                    <a class="social-item" href="javascript:void(0)"
-                    ><i class="fa-brands fa-github"></i
-                        ></a>
-                    <a class="social-item" href="javascript:void(0)"
-                    ><i class="fa-brands fa-telegram"></i
-                        ></a>
+                    <a class="social-item" href="javascript:void(0)">
+                        <i class="fa-brands fa-facebook-f"></i>
+                    </a>
+                    <a class="social-item" href="javascript:void(0)">
+                        <i class="fa-brands fa-google-plus-g"></i>
+                    </a>
+                    <a class="social-item" href="javascript:void(0)">
+                        <i class="fa-brands fa-github"></i>
+                    </a>
+                    <a class="social-item" href="javascript:void(0)">
+                        <i class="fa-brands fa-telegram"></i>
+                    </a>
                 </div>
             </div>
         </div>
