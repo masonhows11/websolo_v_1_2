b46761d20b0dee25bf5039c9ca6ca1bf9f6524a4
mason.hows11 <mason.hows11@gmail.com>
2023-06-25 17:54:39 +0330

    edit emmail updated

--- app/Http/Livewire/Front/Dashboard/EditEmail.php
+++ app/Http/Livewire/Front/Dashboard/EditEmail.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire\Front\Dashboard;
 
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
 use Livewire\Component;
 
 class EditEmail extends Component
@@ -11,14 +12,31 @@ class EditEmail extends Component
     public $user;
     public $email;
 
-    public function mount(){
+    public function mount()
+    {
 
         $this->user = Auth::user();
         $this->email = $this->user->email;
     }
 
-    public function save(){
+    protected function rules()
+    {
+        return [
+            'email' => ['required', 'unique:users', 'email'],
+        ];
+    }
 
+    protected $messages = [
+        'email.required' => 'ایمیل  الزامی است',
+        'email.unique' => 'ایمیل وارد شده تکراری است',
+        'email.email' => 'ایمیل وارد شده معتبر نمی باشد',
+
+    ];
+
+    public function save()
+    {
+        $this->validate();
+        
     }
 
     public function render()
