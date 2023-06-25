df7aa407414be35003b064fafa4b3d10cedcf584
mason.hows11 <mason.hows11@gmail.com>
2023-06-25 18:33:43 +0330

    edit email updated 80%

--- app/Http/Livewire/Front/Dashboard/EditEmail.php
+++ app/Http/Livewire/Front/Dashboard/EditEmail.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Livewire\Front\Dashboard;
 
+use App\Events\RegisterUserEvent;
+use App\Models\User;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Validation\Rule;
+use Illuminate\Support\Str;
 use Livewire\Component;
 
 class EditEmail extends Component
@@ -12,11 +15,13 @@ class EditEmail extends Component
     public $user;
     public $email;
 
+
     public function mount()
     {
 
-        $this->user = Auth::user();
+        $this->user = User::where('email',Auth::user()->email)->first();
         $this->email = $this->user->email;
+
     }
 
     protected function rules()
@@ -36,7 +41,24 @@ class EditEmail extends Component
     public function save()
     {
         $this->validate();
-        
+
+        $code = Str::random(6);
+
+        $this->user->email = $this->email;
+        $this->user->code = $code;
+        $this->user->email_verified_at = null;
+        $this->user->updated_at = Carbon::now();
+        $this->user->save();
+
+
+
+        Auth::logout();
+        session()->invalidate();
+        session()->regenerateToken();
+        RegisterUserEvent::dispatch($this->user,$code);
+        session()->flash('success','ایمیل فعال سازی با موفقبت ارسال شد.');
+        return redirect()->route('verify.email');
+
     }
 
     public function render()
