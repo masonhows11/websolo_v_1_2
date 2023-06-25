3592391cd713b7dcd9b0d95ff592828a4247f402
mason.hows11 <mason.hows11@gmail.com>
2023-06-25 17:39:00 +0330

    edit info profile fixed

--- app/Http/Livewire/Front/Dashboard/EditProfile.php
+++ app/Http/Livewire/Front/Dashboard/EditProfile.php
@@ -4,6 +4,7 @@ namespace App\Http\Livewire\Front\Dashboard;
 
 
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
 use Livewire\Component;
 
 class EditProfile extends Component
@@ -16,13 +17,58 @@ class EditProfile extends Component
 
     public function mount()
     {
-      $this->user = Auth::user();
-      $this->name = $this->name->name;
-      $this->first_name = $this->name->first_name;
-      $this->last_name = $this->name->last_name;
+        $this->user = Auth::user();
+        $this->name = $this->user->name;
+        $this->first_name = $this->user->first_name;
+        $this->last_name = $this->user->last_name;
     }
 
-    public function save(){
+    protected function rules()
+    {
+        return [
+            'name' => ['required', Rule::unique('users')->ignore($this->user->id), 'min:2', 'max:30'],
+            'first_name' => ['nullable', 'min:2', 'max:30'],
+            'last_name' => ['nullable', 'min:2', 'max:30'],
+        ];
+    }
+
+    protected $messages = [
+        'name.required' => 'نام کاربری الزامی است',
+        'name.unique' => 'نام کاربری تکراری است',
+        'name.min' => 'حداقل ۲ کاراکتر وارد کنید',
+        'name.max' => 'حداکثر تعداد کاراکتر مجاز',
+
+
+        'first_name.min' => 'حداقل ۲ کاراکتر وارد کنید',
+        'first_name.max' => 'حداکثر تعداد کاراکتر مجاز',
+
+
+        'last_name.min' => 'حداقل ۲ کاراکتر وارد کنید',
+        'last_name.max' => 'حداکثر تعداد کاراکتر مجاز',
+
+    ];
+
+    public function save()
+    {
+        $this->validate();
+
+        try {
+
+            $this->user->name = $this->name;
+            $this->user->first_name = $this->first_name;
+            $this->user->last_name = $this->last_name;
+            $this->user->save();
+
+            $this->dispatchBrowserEvent('show-result',
+                ['type'=>'success' ,
+                 'message'=>'عملیات با موفقیت انجام شد.']);
+        }catch (\Exception $ex){
+
+            $this->dispatchBrowserEvent('show-result',
+                ['type'=>'error' ,
+                    'message'=>'عملیات با موفقیت انجام نشد.']);
+        }
+
 
     }
 
--- resources/views/livewire/front/dashboard/edit-profile.blade.php
+++ resources/views/livewire/front/dashboard/edit-profile.blade.php
@@ -2,62 +2,85 @@
     @section('page_title')
         ویرایش پروفایل کاربری
     @endsection
-        <div class="container">
-            <div class="col-xl-12 col-lg-12 col-md-10  dash-index">
+    <div class="container">
+        <div class="col-xl-12 col-lg-12 col-md-10  dash-index">
 
-                <div class="row my-3">
-                    <form wire:submit.prevent="save">
+            <div class="row my-3">
+                <form wire:submit.prevent="save">
 
-                        <div class="dash-index-info-title mt-3 mb-5 py-2 px-3">
-                            اطلاعات کاربر
+                    <div class="dash-index-info-title mt-3 mb-5 py-2 px-3">
+                        اطلاعات کاربر
+                    </div>
+
+                    <input type="hidden" value="{{ $user->id }}" name="user">
+                    <div class="col-xl-8 col-lg-8 my-3">
+                        <label for="user-name" class="form-label">نام کاربری :</label>
+                        <input type="text"
+                               wire:model.defer="name"
+                               class="form-control @error('name')is-invalid @enderror"
+                               id="name">
+                        @error('name')
+                        <div class="alert alert-danger my-2">
+                            {{ $message }}
                         </div>
+                        @enderror
+                    </div>
 
-                        <input type="hidden" value="{{ $user->id }}" name="user">
-                        <div class="col-xl-8 col-lg-8 my-3">
-                            <label for="user-name" class="form-label">نام کاربری :</label>
-                            <input type="text"
-                                   wire:model.defer="name"
-                                   class="form-control @error('name')is-invalid @enderror"
-                                   id="name">
-                            @error('name')
-                            <div class="alert alert-danger my-2">
-                                {{ $message }}
-                            </div>
-                            @enderror
+                    <div class="col-xl-8 col-lg-8  my-3">
+                        <label for="first-name" class="form-label">نام :</label>
+                        <input type="text"
+                               wire:model.defer="first_name"
+                               class="form-control @error('first_name') is-invalid @enderror"
+                               id="first-name">
+                        @error('first_name')
+                        <div class="alert alert-danger my-2">
+                            {{ $message }}
                         </div>
+                        @enderror
+                    </div>
 
-                        <div class="col-xl-8 col-lg-8  my-3">
-                            <label for="first-name" class="form-label">نام :</label>
-                            <input type="text"
-                                   wire:model.defer="first_name"
-                                   class="form-control @error('first_name') is-invalid @enderror"
-                                   id="first-name">
-                            @error('first_name')
-                            <div class="alert alert-danger my-2">
-                                {{ $message }}
-                            </div>
-                            @enderror
+                    <div class="col-xl-8 col-lg-8 my-3">
+                        <label for="last-name" class="form-label">نام خانوادگی :</label>
+                        <input type="text"
+                               wire:model.defer="last_name"
+                               class="form-control @error('last_name') is-invalid @enderror"
+                               id="last-name">
+                        @error('last_name')
+                        <div class="alert alert-danger my-2">
+                            {{ $message }}
                         </div>
-
-                        <div class="col-xl-8 col-lg-8 my-3">
-                            <label for="last-name" class="form-label">نام خانوادگی :</label>
-                            <input type="text"
-                                   wire:model.defer="last_name"
-                                   class="form-control @error('last_name') is-invalid @enderror"
-                                   id="last-name">
-                            @error('last_name')
-                            <div class="alert alert-danger my-2">
-                                {{ $message }}
-                            </div>
-                            @enderror
-                        </div>
+                        @enderror
+                    </div>
 
 
-                        <button type="submit" class="btn btn-success">ویرایش</button>
-                    </form>
-                </div>
-
-
+                    <button type="submit" class="btn btn-success">ویرایش</button>
+                </form>
             </div>
+
+
         </div>
+    </div>
 </div>
+@push('front_custom_scripts')
+    <script>
+        const Toast = Swal.mixin({
+            toast: true,
+            position: 'top',
+            showConfirmButton: false,
+            showCloseButton: true,
+            timer: 5000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+                toast.addEventListener('mouseenter', Swal.stopTimer)
+                toast.addEventListener('mouseleave', Swal.resumeTimer)
+            }
+        });
+        window.addEventListener('show-result', ({detail: {type, message}}) => {
+            Toast.fire({
+                icon: type,
+                title: message
+            })
+        })
+    </script>
+@endpush
+
