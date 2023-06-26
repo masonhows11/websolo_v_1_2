6566105eca24d2840af6cb6bd1fbf66fb66cfabd
mason.hows11 <mason.hows11@gmail.com>
2023-06-26 22:45:08 +0330

    delete account updated 90%

--- app/Http/Livewire/Front/Dashboard/DeleteAccount.php
+++ app/Http/Livewire/Front/Dashboard/DeleteAccount.php
@@ -2,10 +2,31 @@
 
 namespace App\Http\Livewire\Front\Dashboard;
 
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class DeleteAccount extends Component
 {
+
+    public $user;
+
+    public function mount(){
+
+        $this->user = Auth::user();
+    }
+
+
+    public function delete(){
+
+        $this->user->delete();
+        Auth::logout();
+        session()->invalidate();
+        session()->regenerateToken();
+        session()->flash('success','حساب کاربری شما با موفقیت حذف شد.');
+        redirect()->route('login.form');
+
+    }
+
     public function render()
     {
         return view('livewire.front.dashboard.delete-account')
--- resources/views/livewire/front/dashboard/delete-account.blade.php
+++ resources/views/livewire/front/dashboard/delete-account.blade.php
@@ -4,21 +4,24 @@
     @endsection
     <div class="container">
 
-        <div class="dash-index-info-title mt-3 mb-5 py-2 px-3">
-            حذف حساب کاربری
+        <div class="col-xl-12 col-lg-12 col-md-10  dash-index">
+
+            <div class="dash-index-info-title mt-3 mb-5 py-2 px-3">
+                حذف حساب کاربری
+            </div>
+
+            <div class="row my-3">
+                <form wire:submit.prevent="delete">
+                    <div class="mb-3 d-flex flex-column">
+                        <label for="delete-account" class="form-label my-4">حذف حساب کاربری</label>
+                        <button type="submit" id="delete-account" class="btn btn-danger btn-sm">حذف حساب کاربری {{ $user->email }}</button>
+                    </div>
+                </form>
+            </div>
+
         </div>
 
-        <div class="row my-3">
 
-            <form wire:submit.prevent="delete">
-
-                <div class="mb-3">
-                    <button type="submit" class="btn btn-danger btn-sm">حذف حساب کاربری</button>
-                </div>
-
-            </form>
-
-        </div>
 
     </div>
 </div>
