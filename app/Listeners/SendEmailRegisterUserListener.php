33a515616121078f1e7ed2c4836916155e6be670
mason.hows11 <mason.hows11@gmail.com>
2023-06-27 19:17:07 +0330

    add send email reg new user & resend it with default queue

--- app/Listeners/SendEmailRegisterUserListener.php
+++ app/Listeners/SendEmailRegisterUserListener.php
@@ -4,23 +4,22 @@ namespace App\Listeners;
 
 use App\Events\RegisterUserEvent;
 use App\Mail\VerifyEmailNewUser;
+use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Mail;
 
-class SendEmailRegisterUserListener
+class SendEmailRegisterUserListener implements ShouldQueue
 {
 
+    use Queueable;
+
     //  use InteractsWithQueue;
-    
+
     //    public $connection = 'sqs';
-    //
+
     //    public $queue = 'send_user_email_reg';
-    //
-    //
-    //    public $delay = 10;
-    //
-    //    public $tries = 3;
+
 
     /**
      * Create the event listener.
