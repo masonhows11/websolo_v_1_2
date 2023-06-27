a645c1cd2a10a9bf04608e3b08df987cb18e77ed
mason.hows11 <mason.hows11@gmail.com>
2023-06-27 18:06:29 +0330

    validation email updated

--- app/Http/Controllers/Auth/ResendEmailVerifyController.php
+++ app/Http/Controllers/Auth/ResendEmailVerifyController.php
@@ -10,41 +10,40 @@ use Illuminate\Support\Str;
 
 class ResendEmailVerifyController extends Controller
 {
-    public function resendEmailPrompt(){
+    public function resendEmailPrompt()
+    {
 
         return view('auth_front.verify_email_prompt');
     }
 
-    public function resendEmail(Request $request){
+    public function resendEmail(Request $request)
+    {
 
         $request->validate([
-            'email' => ['required','email'],
+            'email' => ['required', 'email'],
 
         ], $messages = [
             'email.required' => 'ایمیل الزامی است.',
             'email.email' => 'ایمیل معتبر نیست.',
         ]);
 
+        $user = User::where('email', $request->email)->first();
+        if (!$user) {
+            session()->flash('error', 'ایمیل وارد شده وجود ندارد.');
+            return redirect()->back();
+        }
         try {
-            $user = User::where('email',$request->email)->first();
-            if (!$user){
-                session()->flash('error','ایمیل وارد شده وجود ندارد.');
-                return  redirect()->back();
-            }
 
             $code = Str::random(6);
             $user->code = $code;
             $user->save();
-            RegisterUserEvent::dispatch($user,$code);
-            session()->flash('success','ایمیل فعال سازی با موفقبت ارسال شد.');
+            RegisterUserEvent::dispatch($user, $code);
+            session()->flash('success', 'ایمیل فعال سازی با موفقبت ارسال شد.');
             return redirect()->route('verify.email');
 
-        }catch (\Exception $ex){
-            return view('errors_custom.general_error',['error'=>$ex->getMessage()]);
+        } catch (\Exception $ex) {
+            return view('errors_custom.general_error', ['error' => $ex->getMessage()]);
         }
-
-
-
 
 
     }
