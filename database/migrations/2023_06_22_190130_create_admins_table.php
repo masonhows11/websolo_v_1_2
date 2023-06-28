f7c5992796a5f5a185a804603f0655ff7aa431fb
mason.hows11 <mason.hows11@gmail.com>
2023-06-28 20:51:34 +0330

    admin profile updated

--- database/migrations/2023_06_22_190130_create_admins_table.php
+++ database/migrations/2023_06_22_190130_create_admins_table.php
@@ -25,6 +25,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->timestamp('mobile_verified_at')->nullable();
             $table->string('password')->nullable();
+            $table->rememberToken();
             $table->timestamps();
         });
     }
