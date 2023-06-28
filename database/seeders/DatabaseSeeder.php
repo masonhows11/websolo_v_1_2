f507287f5a8939159bb829e8d51fbb162dccfdc3
mason.hows11 <mason.hows11@gmail.com>
2023-06-28 14:54:59 +0330

    add seeder for admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Admin;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\PermissionRegistrar;
 
 class DatabaseSeeder extends Seeder
 {
@@ -19,19 +23,22 @@ class DatabaseSeeder extends Seeder
 
         app()[PermissionRegistrar::class]->forgetCachedPermissions();
         $admin = Admin::create([
-            'name' => 'naeem2384',
+            'name' => 'naeem927',
             'first_name' => 'naeem',
             'last_name' => 'soltany',
             'mobile' => '09917230927',
-            'email' => 'mason.hows11@gmail.com'
+            'email' => 'mason.hows11@gmail.com',
+            'email_verified_at' => Carbon::now(),
             //'token'=>  mt_rand(111111,999999),
-            //'token_verified_at' => Carbon::now(),
+            //'email_verified_at' => Carbon::now(),
         ]);
         $admin1 = Admin::create([
-            'name' => 'james_423',
+            'name' => 'james423',
             'first_name' => 'joe',
             'last_name' => 'james',
             'mobile' => '09172890423',
+            'email' => 'joe.james@gmail.com',
+            'email_verified_at' => Carbon::now(),
             //'token'=>  mt_rand(111111,999999),
             //'token_verified_at' => Carbon::now(),
         ]);
