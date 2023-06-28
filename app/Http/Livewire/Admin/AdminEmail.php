caca9d0f4159e1a3b2ca02797b05779ae13717bf
mason.hows11 <mason.hows11@gmail.com>
2023-06-28 21:20:43 +0330

    admin change email updated

--- app/Http/Livewire/Admin/AdminEmail.php
+++ app/Http/Livewire/Admin/AdminEmail.php
@@ -2,12 +2,63 @@
 
 namespace App\Http\Livewire\Admin;
 
+use App\Notifications\AdminAuthNotification;
+use App\Services\GenerateToken;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class AdminEmail extends Component
 {
+    public $name;
+    public $email;
+
+
+    public function mount()
+    {
+        $info = Auth::guard('admin')->user();
+        $this->name = $info->name;
+        $this->email = $info->email;
+
+    }
+
+    protected function rules()
+    {
+        return [
+            'email' => ['unique:admins','required']
+        ];
+    }
+
+
+    protected $messages =
+        [
+            'email.unique' => 'ایمیل وارد شده تکراری است',
+            'email.required' => 'ایمیل الزامی است',
+        ];
+
+    public function editMobile()
+    {
+        $this->validate();
+        $code = GenerateToken::generateToken();
+        try {
+            $admin = Auth::user();
+            $admin->email = $this->email;
+            $admin->code = $code;
+            $admin->email_verified_at = null;
+            $admin->remember_token = null;
+            $admin->save();
+
+            $admin->notify(new AdminAuthNotification($admin->email,$code));
+
+            session()->flash('success', 'کد فعال سازی به ایمیل ارسال شد');
+            return redirect()->route('admin.validate.email.form');
+        }catch (\Exception $ex){
+            return view('errors_custom.model_store_error',['error'=>$ex->getMessage()]);
+        }
+    }
     public function render()
     {
-        return view('livewire.admin.admin-email');
+        return view('livewire.admin.admin-email')
+            ->extends('admin.include.master')
+            ->section('admin_main');
     }
 }
--- resources/views/livewire/admin/admin-email.blade.php
+++ resources/views/livewire/admin/admin-email.blade.php
@@ -1,3 +1,36 @@
 <div>
-    {{-- Because she competes with no one, no one can compete with her. --}}
+    @section('admin_title')
+        پروفایل کاربری
+    @endsection
+    @section('breadcrumb')
+{{--        {{ Breadcrumbs::render('admin.mobile') }}--}}
+    @endsection
+    <div class="container">
+        <div class="row admin-mobile-section">
+            <div class="col-xl-5 col-lg-5 col-md-5">
+                <form wire:submit.prevent="editMobile">
+                    <div class="mb-3 mt-3">
+                        <label for="name" class="form-label">{{ $name }}</label>
+                    </div>
+                    <div class="mb-3 mt-3">
+                        <label for="email" class="form-label">ایمیل:</label>
+                        <input type="text" class="form-control" wire:model.defer="email" id="email">
+                        @error('email')
+                        <div class="alert alert-danger my-2">
+                            {{ $message }}
+                        </div>
+                        @enderror
+                    </div>
+
+                    <div class="form-group my-4">
+                        <input type="submit" class="btn btn-success" value="تایید">
+                        <a href="{{ route('admin.profile') }}" class="btn btn-primary">بازگشت</a>
+                    </div>
+
+                </form>
+            </div>
+
+
+        </div>
+    </div>
 </div>
