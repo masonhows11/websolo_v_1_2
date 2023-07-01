359f827f416ab5fcfeef118bfee8ad7bb4d74056
mason.hows11 <mason.hows11@gmail.com>
2023-07-01 17:29:45 +0330

    add front back model & ready for cruds

---
+++ app/Http/Livewire/Admin/AdminBackend.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Admin;
+
+use Livewire\Component;
+
+class AdminBackend extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.admin-backend');
+    }
+}
