9746ec37457ec39d2d2a99eaae60b8da70fbf13f
mason.hows11 <mason.hows11@gmail.com>
2023-07-06 21:51:46 +0330

    sample front style updated

--- resources/views/livewire/front/samples/sample-component.blade.php
+++ resources/views/livewire/front/samples/sample-component.blade.php
@@ -1,80 +1,81 @@
 <div>
 
-@section('page_title')
-@endsection
-    <div class="container">
-    @if ($sample)
+    @section('page_title')
+    @endsection
+    <div class="container ws-sample-page-main">
+    @if($sample != null )
         <!-- main info -->
-            <div class="row my-3 p-3  w3-flat-clouds rounded-3">
+            <div class="row my-3  w3-flat-clouds rounded-3">
 
-                <div class="col-sm-6 py-4  d-flex flex-column ">
-                    <div class="">
-                        <h3>{{ $sample->title_persian }}</h3>
-                        <h5>{{ $sample->title_english }}</h5>
-                    </div>
+                <div class="col py-4 d-flex justify-content-center">
                     <img src="{{ asset('storage/samples/'. $sample->main_image) }}" loading="lazy"
-                         class="img-thumbnail h-100" alt="sample-main-image">
+                         class="h-100" alt="sample-main-image">
                 </div>
-                <div class="col-sm-6 py-2 d-flex flex-column   align-content-center justify-content-around">
 
-                    <h4 class="text-black">جزئیات توسعه پروژه :</h4>
-                    <div class=" py-3">
+                <div class="col py-2 d-flex flex-column">
+                    <div class="mt-4">
+                        <h3 class="my-2 h3">{{ $sample->title_persian }}</h3>
+                        <h5 class="my-2 h4" >{{ $sample->title_english }}</h5>
+                    </div>
+                    <div class="mt-4">
+                        <h4 class="text-black">جزئیات توسعه پروژه :</h4>
+                    </div>
+                    <div class="py-3 mt-2">
                         <h5>بک اند (back-end)</h5>
                         @foreach ($sample->backEnds as $lng)
-                            <ul class="list-group  list-group-flush list-group-horizontal my-2 py-2">
+                            <ul class="list-group  ws-sample-back-end-list list-group-flush list-group-horizontal my-2 py-2">
                                 <li class="">{{ $lng->title_persian }}</li>
                             </ul>
                         @endforeach
                         <h5>فرانت اند (front-end)</h5>
                         @foreach ($sample->frontEnds as $lng)
-                            <ul class="list-group list-group-flush list-group-horizontal my-2 py-2">
+                            <ul class="list-group  ws-sample-front-end-list list-group-flush list-group-horizontal my-2 py-2">
                                 <li class="">{{ $lng->title_persian }}</li>
                             </ul>
                         @endforeach
                     </div>
-
-                    <div class="">
-                        <div class="row">
-                            <h4>خلاصه :</h4>
-                            <div class="">
-                                {!! $sample->short_description !!}
-                            </div>
+                    <div class="mt-5">
+                        <h4>خلاصه :</h4>
+                        <div class="">
+                            {!! $sample->short_description !!}
                         </div>
-                        <div class="row">
-                            <div class="col">
-                                <p class="">ایجاد شده در تاریخ
-                                    : {{ JDate($sample->created_at)->format('Y/m/d') }}</p>
-                            </div>
-
-                            <div class="col ">
-                                <span class="">{{ $sample->views }}</span>
-                                <i class="fa-regular fa-eye"></i>
-                                <span class=""
-                                      id="like-count">{{ $sample->likes()->count() }}</span>
-
-                                @auth
-                                    @if (\Illuminate\Support\Facades\Auth::user()->likes()->where(['sample_id' => $sample->id, 'like' => 1])->first())
-                                        <i class="fas fa-heart fa-border-style" style="color:tomato" id="add-like"
-                                           data-sample="{{ $sample->id }}"></i>
-                                    @else
-                                        <i class="far fa-heart" style="color:tomato" id="add-like"
-                                           data-sample="{{ $sample->id }}"></i>
-                                    @endif
-                                @else
-                                    <i class="far fa-heart fa-border-style" style="color:tomato"
-                                       id="add-like-an-auth" data-sample="{{ $sample->id }}"></i>
-                                @endauth
-                            </div>
-                        </div>
+                    </div>
+                    <div class="mt-5">
+                        <p class="">ایجاد شده در تاریخ
+                            : {{ JDate($sample->created_at)->format('Y/m/d') }}</p>
                     </div>
                 </div>
             </div>
 
+
+
             <!-- description -->
-            <h4 class="mt-3"> توضیحات</h4>
-            <div class="row   w3-flat-clouds rounded-3">
+            <h4 class="my-3"> توضیحات</h4>
+            <div class="row d-flex flex-column  w3-flat-clouds rounded-3">
+
                 <div class="col mx-3 py-5">
                     {!! $sample->description !!}
+                </div>
+
+                <div class="col">
+                    <div class="d-flex justify-content-end">
+                        <span class="">{{ $sample->views }}</span>
+                        <i class="fa-regular fa-eye"></i>
+                        <span class=""
+                              id="like-count">{{ $sample->likes()->count() }}</span>
+                        @auth
+                            @if (\Illuminate\Support\Facades\Auth::user()->likes()->where(['sample_id' => $sample->id, 'like' => 1])->first())
+                                <i class="fas fa-heart fa-border-style" style="color:tomato" id="add-like"
+                                   data-sample="{{ $sample->id }}"></i>
+                            @else
+                                <i class="far fa-heart" style="color:tomato" id="add-like"
+                                   data-sample="{{ $sample->id }}"></i>
+                            @endif
+                        @else
+                            <i class="far fa-heart fa-border-style" style="color:tomato"
+                               id="add-like-an-auth" data-sample="{{ $sample->id }}"></i>
+                        @endauth
+                    </div>
                 </div>
             </div>
             <!-- image gallery -->
@@ -207,7 +208,7 @@
         @endif
 
 
-       <div class="row d-flex justify-content-center  my-4">
+        <div class="row d-flex justify-content-center  my-4">
             <div class="col-xxl-5 col-xl-5 col-lg-5 col-md-4 col-10">
                 <form id="add-comment">
                     @auth
@@ -263,3 +264,5 @@
 
     </div>
 </div>
+
+
