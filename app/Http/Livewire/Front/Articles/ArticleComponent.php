22d8c82e27453a0651e35c0275d58396225856aa
mason.hows11 <mason.hows11@gmail.com>
2023-07-06 17:28:13 +0330

    add single article sample component

---
+++ app/Http/Livewire/Front/Articles/ArticleComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Front\Articles;
+
+use Livewire\Component;
+
+class ArticleComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.front.articles.article-component');
+    }
+}
---
+++ app/Http/Livewire/Front/Samples/SampleComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Front\Samples;
+
+use Livewire\Component;
+
+class SampleComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.front.samples.sample-component');
+    }
+}
