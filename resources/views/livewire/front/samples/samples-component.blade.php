aee0b611691833402eec40625c53754f72613475
mason.hows11 <mason.hows11@gmail.com>
2023-07-06 22:31:33 +0330

    front sample style updated

--- resources/views/livewire/front/samples/samples-component.blade.php
+++ resources/views/livewire/front/samples/samples-component.blade.php
@@ -3,27 +3,27 @@
         نمونه کارها
     @endsection
     <div class="container">
-        <div class="row row-cols-xxl-4 row-cols-xl-4 row-cols-lg-3 row-cols-md-2 row-cols-1 my-3 py-4 px-4  sample-page">
+        <div class="row row-cols-xxl-4 row-cols-xl-4 row-cols-lg-3 row-cols-md-2 row-cols-1 my-3 py-4 px-4  ws-samples-page-main">
             @if ($samples->count())
                 @foreach ($samples as $sample)
                     <div class="col my-2">
-                        <div class="wk-article-card d-flex flex-column h-100">
-                            <div class="wk-article-img-card">
+                        <div class="ws-article-card d-flex flex-column h-100">
+                            <div class="ws-article-img-card">
                                 <img src="{{ asset('storage/samples/'. $sample->main_image) }}"
                                      class="img-fluid rounded-4" alt="sample-image"/>
                             </div>
-                            <div class="wk-article-card-body">
-                                <div class="wk-article-card-title  pt-3"><h5>{{ $sample->title_persian }}</h5></div>
-                                <div class="wk-article-card-text my-1">
+                            <div class="ws-article-card-body">
+                                <div class="ws-article-card-title  pt-3"><h5>{{ $sample->title_persian }}</h5></div>
+                                <div class="ws-article-card-text my-1">
                                     <div class="desc">
                                         {!! $sample->short_description !!}
                                     </div>
                                 </div>
-                                <div class="wk-article-card-footer d-flex justify-content-between my-1">
-                                    <div class="py-2 px-2 wk-article-date"><i
+                                <div class="ws-article-card-footer d-flex justify-content-between my-1">
+                                    <div class="py-2 px-2 ws-article-date"><i
                                             class="fa-regular fa-clock"></i>{{ jDate($sample->created_at)->ago()  }}
                                     </div>
-                                    <a class="btn wk-article-continue" href="{{ route('sample', [$sample->slug]) }}">ادامه....</a>
+                                    <a class="btn ws-article-continue" href="{{ route('sample', [$sample->slug]) }}">ادامه....</a>
                                 </div>
                             </div>
                         </div>
