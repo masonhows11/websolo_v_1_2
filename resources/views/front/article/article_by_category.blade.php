da33768569f8e677922c169a6372744f14a1e23b
mason.hows11 <mason.hows11@gmail.com>
2023-07-10 18:16:24 +0330

    artilces front style updated

--- resources/views/front/article/article_by_category.blade.php
+++ resources/views/front/article/article_by_category.blade.php
@@ -5,6 +5,61 @@
 @section('main_content')
     <div class="container">
 
+        <div class="row mt-5">
+
+            <div class="col-md-2 col-sm-2 mt-2 article-category  w3-flat-clouds rounded-3">
+                @include('front.include.category')
+            </div>
+
+            <div class="col-md-10">
+                <div class="row  row-cols-xxl-3 row-cols-xl-3 row-cols-lg-3 row-cols-md-2 row-cols-1">
+                    @if ($articles->count())
+                        @foreach ($articles as $article)
+                            <div class="col my-2">
+                                <div class="ws-article-card d-flex flex-column h-100">
+                                    <div class="ws-article-img-card">
+                                        <img src="{{ asset('storage/articles/' . $article->image) }}"
+                                             class="img-fluid rounded" alt="article-image"/>
+                                    </div>
+
+                                    <div class="ws-article-card-body my-3">
+                                        <div class="ws-article-card-title pt-3"><h5
+                                                class="h5">{{ $article->title_persian }}</h5></div>
+                                        <div class="ws-article-card-text my-1">
+                                            <div class="desc">
+                                                {!! $article->short_description !!}
+                                            </div>
+                                        </div>
+                                        <div class="ws-article-card-footer d-flex justify-content-between my-1">
+                                            <div class="py-2 px-2 ws-article-date">
+                                                <i class="fa-regular fa-clock me-2"></i>
+                                                {{ jDate($article->created_at)->ago()  }}
+                                            </div>
+                                            <a class="ws-article-continue-btn me-3 pt-2"
+                                               href="{{ route('article', ['article'=>$article->slug]) }}">ادامه....</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        @endforeach
+                    @endif
+                </div>
+            </div>
+        </div>
+        <div class="row d-flex justify-content-center mt-5">
+            <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-4 d-flex justify-content-center">
+                {{ $articles->links('front.paginate.paginate') }}
+            </div>
+        </div>
+    </div>
+@endsection
+{{-- @extends('front.include.master_front')
+@section('page_title')
+    مقالات
+@endsection
+@section('main_content')
+    <div class="container">
+
         <div class="ws-articles-wrapper mt-5">
 
             <div class="article-category my-3 w3-flat-clouds rounded-3" >
@@ -53,7 +108,7 @@
         </div>
 
     </div>
-@endsection
+@endsection --}}
 
 {{--@extends('front.include.master_front')
 @section('page_title')
@@ -108,3 +163,4 @@
 
     </div>
 @endsection--}}
+
--- resources/views/front/include/child_category.blade.php
+++ resources/views/front/include/child_category.blade.php
@@ -1,5 +1,5 @@
 @foreach ($category as $child)
-    <li class="mt-4 mb-4 px-5 child-category">
+    <li class="mt-4 mb-4  child-category">
         <a href="{{ route('article.category',[$child]) }}">{{ $child->title_persian }}</a></li>
     <ul class="ps-2">
         @if (count($child->children))
